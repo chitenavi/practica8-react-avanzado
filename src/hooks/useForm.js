7147ee0883f5a61c1c58974743d29465fd6129dd
Ivan Chinea <chitenavi@gmail.com>
2021-01-08 11:39:38 +0000

    Update useForm for checkboxes

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -3,8 +3,12 @@ import { useState } from 'react';
 const useForm = (...formFields) => {
   const [form, setForm] = useState(...formFields);
 
-  const handleFormChange = event =>
-    setForm({ ...form, [event.target.name]: event.target.value });
+  const handleFormChange = event => {
+    if (event.target.type === 'checkbox') {
+      return setForm({ ...form, [event.target.name]: event.target.checked });
+    }
+    return setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
   return [form, handleFormChange];
 };
